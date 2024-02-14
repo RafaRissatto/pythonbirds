f957fb927bd113fbd58f96af2822318152a53add
Rafael <rafaelrissatto9@gmail.com>
2024-02-14 17:54:07 -0300

    Criado atributos de insatancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,15 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=34):
+        self.idade = idade
+        self.nome = nome
+
+    def comprimentar(self):
+        return f'Olá {id(self)}'
+
+p = Pessoa()
+print(Pessoa.comprimentar(p))
+print(p.comprimentar())
+print(p.nome)
+p.nome = 'Rafael'
+print(p.nome)
+print(p.idade )
